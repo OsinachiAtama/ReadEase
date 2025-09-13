ce36314079d1ae9029212de6aeb75e9d2ab7b8b9
OsinachiAtama <osinachiatama@gmail.com>
2025-09-13 13:10:52 -0400

    Emptying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,1 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import render_get_start from './pages/getting_started'
-import RenderGetStart from './pages/getting_started'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-    return (
-    <>
-      <RenderGetStart/> 
-    </>
-  );
-}
-
-
-export default App
